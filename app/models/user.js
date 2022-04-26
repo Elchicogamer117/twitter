f401beb54c0b891569422f82935d53b90ea64da9
Cristian <cris18002011@hotmail.com>
2022-04-26 12:03:08 -0500

    TDD - Test requerimiento 2

--- app/models/user.js
+++ app/models/user.js
@@ -1,15 +1,15 @@
 // Requerimiento 1: Se necesita un modelo `User` para guardar la información de un usuario de nuestra app:
 // `id`, `username`, `name`, `bio`, `dateCreated` y `lastUpdated`. 
 
-// class user{
-//       constructor(id, username, name, bio, dataCreated, lastUpdated){
-//             this.id = id
-//             this.username = username
-//             this.name = name
-//             this.bio = bio
-//             this.dataCreated = dataCreated
-//             this.lastUpdated = lastUpdated
-//       }
-// }
+class user{
+      constructor(id, username, name, bio, dataCreated, lastUpdated){
+            this.id = id
+            this.username = username
+            this.name = name
+            this.bio = bio
+            this.dataCreated = new Date()
+            this.lastUpdated = new Date()
+      }
+}
 
-// module.exports = user
+module.exports = user
--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,18 +1,18 @@
-// Requerimiento 1: Se necesita un modelo `User` para guardar la información de un usuario de nuestra app:
-// `id`, `username`, `name`, `bio`, `dateCreated` y `lastUpdated`. 
-//const { describe } = require('yargs')
+// Requerimiento 1: Se necesita un modelo `User` para guardar la información de un usuario de nuestra app:`id`, `username`, `name`, `bio`, `dateCreated` y `lastUpdated`. 
+// Requerimiento 2: Las propiedades `dateCreated` y `lastUpdated` deberán ser datos de tipo fecha que guarden el momento en que se instancie un nuevo objeto de la clase `User`.
+
 const User = require('./../../app/models/user')
 
 describe('Unit test for user class', () => {
       test('1) Create an user object', () => {
-            const user = new User( 1, 'elchicogamer', 'Cristian', 'Bio', 'dataCreated', 'lastUpdate')
+            const user = new User( 1, 'elchicogamer', 'Cristian', 'Bio')
             
             expect(user.id).toBe(1)
             expect(user.username).toBe('elchicogamer')
             expect(user.name).toBe('Cristian')
             expect(user.bio).toBe('Bio')
-            expect(user.dataCreated).toBe('dataCreated')
-            expect(user.lastUpdated).toBe('lastUpdate')
+            expect(user.dataCreated).not.toBeUndefined()
+            expect(user.lastUpdated).not.toBeUndefined()
       })
 
 })
