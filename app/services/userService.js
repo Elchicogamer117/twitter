81db29d3f2692e93e8775c761c7a524a4b6e682e
Cristian <cris18002011@hotmail.com>
2022-04-26 13:39:02 -0500

    Sprint 2: Test requerimiento 2

--- app/services/userService.js
+++ app/services/userService.js
@@ -4,6 +4,11 @@ class userService{
       static create(id, username, name){
             return new user(id, username, name, 'bio default')
       }
+      static getinfo(user){
+            return [user.id, user.username, user.name, 'bio default']
+      }
+
 }
+
 
 module.exports = userService
--- test/services/userService.test.test.js
+++ test/services/userService.test.test.js
@@ -2,6 +2,7 @@
 // Criterios de aceptación:
 // 1. Esta clase deberá tener un método `create` que se pueda usar sin instanciar (static method), y que reciba solo los parámetros del `id`, `username` y `name`. 
 // 2. El valor de `bio` deberá ser por default para todos los `user` creados. 
+//Requerimiento 2: Agregar un nuevo método estático en `UserService` llamado `getInfo` que al recibir un objeto de la clase `User`, me regrese una lista con todos los valores de los atributos de dicho objeto.
 
 const userService = require('./../../app/services/userService')
 
@@ -14,5 +15,16 @@ describe('Test for UserService', () =>{
             expect(user.name).toBe('Cristian')
             expect(user.id).toBe(2)
             expect(user.bio).not.toBeUndefined()
+      })
+
+      test('2) Get all user data in a list', () => { 
+
+            const user = userService.create(2, 'cris18002011', 'Cristian')
+            const userInfoInList = userService.getinfo(user) 
+
+            expect(userInfoInList[0]).toBe(2)
+            expect(userInfoInList[1]).toBe('cris18002011')
+            expect(userInfoInList[2]).toBe('Cristian')
+            expect(userInfoInList[3]).toBe('bio default')
       })
 })
