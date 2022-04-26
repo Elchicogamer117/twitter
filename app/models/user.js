57431bc81e819ab058b5988a1423b7c1efe660a7
Cristian <cris18002011@hotmail.com>
2022-04-26 12:14:08 -0500

    TDD - Test requerimiento 3

--- app/models/user.js
+++ app/models/user.js
@@ -2,13 +2,26 @@
 // `id`, `username`, `name`, `bio`, `dateCreated` y `lastUpdated`. 
 
 class user{
-      constructor(id, username, name, bio, dataCreated, lastUpdated){
+      constructor(id, username, name, bio, dateCreated, lastUpdated){
             this.id = id
             this.username = username
             this.name = name
             this.bio = bio
-            this.dataCreated = new Date()
+            this.dateCreated = new Date()
             this.lastUpdated = new Date()
+      }
+      get getUsername(){
+            return this.username
+      }
+
+      get getBio(){
+            return this.bio
+      }
+      get getDateCreated(){
+            return this.dateCreated
+      }
+      get getLastUpdated(){
+            return this.lastUpdated
       }
 }
 
--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,5 +1,6 @@
 // Requerimiento 1: Se necesita un modelo `User` para guardar la información de un usuario de nuestra app:`id`, `username`, `name`, `bio`, `dateCreated` y `lastUpdated`. 
 // Requerimiento 2: Las propiedades `dateCreated` y `lastUpdated` deberán ser datos de tipo fecha que guarden el momento en que se instancie un nuevo objeto de la clase `User`.
+// Requerimiento 3: Cada objeto tipo user necesita los siguietes getters: `getUsername`, `getBio`, `getDateCreated`, `getLastUpdated`.
 
 const User = require('./../../app/models/user')
 
@@ -11,8 +12,17 @@ describe('Unit test for user class', () => {
             expect(user.username).toBe('elchicogamer')
             expect(user.name).toBe('Cristian')
             expect(user.bio).toBe('Bio')
-            expect(user.dataCreated).not.toBeUndefined()
+            expect(user.dateCreated).not.toBeUndefined()
             expect(user.lastUpdated).not.toBeUndefined()
       })
 
+      test('2) Add getters', () => {
+            const user = new User( 1, 'elchicogamer', 'Cristian', 'Bio')
+            
+            expect(user.getUsername).toBe('elchicogamer')
+            expect(user.getBio).toBe('Bio')
+            expect(user.getDateCreated).not.toBeUndefined()
+            expect(user.lastUpdated).not.toBeUndefined()
+      })
 })
+
