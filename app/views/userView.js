e4e29dc346046dec69ab679bdf4d80521d4ad6cc
Cristian <cris18002011@hotmail.com>
2022-04-26 23:19:18 -0500

    Sprint 3: Test requerimiento 2

--- app/views/userView.js
+++ app/views/userView.js
@@ -5,8 +5,13 @@ class userView{
             if (payload == null){
                   return {error: 'El payload no existe'}
             }
-            else{
-                  userService.create(payload) //*! <- funciona para la prueba pero no para un correcto funcionamiento, por eso es importante hacer fallar la prueba a proposito
+            // else{
+            //       userService.create(payload) //*! <- funciona para la prueba pero no para un correcto funcionamiento, por eso es importante hacer fallar la prueba a proposito
+            // }
+            else if(typeof payload.username === 'string' && typeof payload.name === 'string' && typeof payload.id === 'number'){
+                  return userService.create(payload.id, payload.username, payload.name)
+            } else{
+                  return {error: "Las propiedades del payload necesitan tener un valor válido"}
             }
       }
 }
